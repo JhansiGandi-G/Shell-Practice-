1f91381293075d881b989f2d78351feba3bb7f1a
JhansiGandi-G <gandi123jhansi@gmail.com>
2026-02-12 16:24:51 +0530

    shell

--- 09-delete-old-log.sh
+++ 09-delete-old-log.sh
@@ -13,7 +13,7 @@ if [ ! -d $LOGS_DIR ]; then
     exit 1
 fi
 
-FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log")
+FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
 # echo "$FILES_TO_DELETE"
 
 if [ -z "$FILES_TO_DELETE" ]; then
