a7c47b835c91dc38d861107d48a0f7fa2d9d64d6
JhansiGandi-G <gandi123jhansi@gmail.com>
2026-02-14 19:43:23 +0530

    shell

---
+++ 11-backup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/backup.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # 14 days is the default value, if the user not supplied
+
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N"
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+USAGE(){
+    log "$R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
+    exit 1
+}
+
+
+if [ $# -lt 2 ]; then
+    USAGE
+fi
+
+if [ ! -d $SOURCE_DIR] then
+    log -e "$R Source directory: $SOURCE_DIR not exists. $N"
+    exist 1
+fi
+
+if [ ! -d $DEST_DIR] then
+    log -e "$R Source directory: $SOURCE_DIR not exists. $N"
+    exist 1
+fi
